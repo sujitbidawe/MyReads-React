eb7b89881fff8a44ae2c139995578b9f7338f73b
sujitbidawe <bidawesujit@gmail.com>
2022-09-04 17:22:51 +0530

    MODIFIED: code to display books from database which are on shelf

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,14 +1,27 @@
 import "./App.css";
+import { useState, useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import SearchBooks from "./components/SearchBooks";
 import DisplayBooks from "./components/DisplayBooks";
+import * as BooksAPI from './BooksAPI';
 
 function App() {
+
+  const [allBooks, setBooks] = useState([]);
+
+  useEffect(() => {
+    const getBooks = async () => {
+      const res = await BooksAPI.getAll();
+      setBooks(res);
+    }
+
+    getBooks();
+  }, [] );
 
   return (
     <Routes>
       <Route exact path="/" element={
-        <DisplayBooks />
+        <DisplayBooks books={allBooks} />
       }
       />
       <Route exact path='/add' element={
