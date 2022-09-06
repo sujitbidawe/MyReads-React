09a3a6bb26a79e0adceff17fe63f1e8c87a3e2f8
sujitbidawe <bidawesujit@gmail.com>
2022-09-06 12:45:09 +0530

    ADDED: page not found component for unknown routes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import SearchBooks from "./components/SearchBooks";
 import Shelves from "./components/Shelves";
 import * as BooksAPI from './BooksAPI';
+import NotFound from "./components/NotFound";
 
 function App() {
 
@@ -42,6 +43,10 @@ function App() {
 			/>
 			<Route exact path='/search' element={
 				<SearchBooks shelfBooks={allBooks} updateBook={(book, newShelf) => updateBook(book, newShelf)} />
+			}
+			/>
+			<Route path='*' element={
+				<NotFound />
 			}
 			/>
 		</Routes>
---
+++ src/components/NotFound.js
@@ -0,0 +1,9 @@
+const NotFound = () => {
+    return (
+        <h2 className="not-found">
+            404! Page Not Found!
+        </h2>
+    )
+}
+
+export default NotFound;
